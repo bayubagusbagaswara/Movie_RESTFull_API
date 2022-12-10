cdf2f1900457e3e58d343e523749583d8ece4909
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:33:45 +0700

    update exception ResourceNotFoundException

--- src/main/java/com/bayu/movie/exception/ResourceNotFoundException.java
+++ src/main/java/com/bayu/movie/exception/ResourceNotFoundException.java
@@ -1,16 +1,44 @@
 package com.bayu.movie.exception;
 
+import com.bayu.movie.dto.MessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
+    private transient MessageResponse messageResponse;
+
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super();
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+    public MessageResponse getMessageResponse() {
+        return messageResponse;
+    }
+
+    public void setMessageResponse() {
+        String message = String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue);
+
+        messageResponse = new MessageResponse(Boolean.FALSE, message);
     }
 }
