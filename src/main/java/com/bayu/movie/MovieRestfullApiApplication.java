d6765c61e21f861e9a208ea9561bfcf0b4363f25
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 20:36:04 +0700

    Initial commit

---
+++ src/main/java/com/bayu/movie/MovieRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRestfullApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/movie/MovieRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
