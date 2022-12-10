16a194d6bbfb065e63093a8d329505e1500378f0
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:16:31 +0700

    add util ValidationUtil

---
+++ src/main/java/com/bayu/movie/util/ValidationUtil.java
@@ -0,0 +1,19 @@
+package com.bayu.movie.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.validation.*;
+import java.util.Set;
+
+@Component
+public class ValidationUtil {
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
+    public void validate(Object object) {
+        Set<ConstraintViolation<Object>> violations = validator.validate(object);
+        if (violations.size() != 0) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
