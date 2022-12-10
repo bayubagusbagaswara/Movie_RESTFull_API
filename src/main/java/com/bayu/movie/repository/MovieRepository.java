282d3ddc42ca350efdbb6c350aa1ea332050867b
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:10:03 +0700

    add repository MovieRepository

---
+++ src/main/java/com/bayu/movie/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.bayu.movie.repository;
+
+import com.bayu.movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
