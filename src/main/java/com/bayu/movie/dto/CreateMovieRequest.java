994938541c7d02380f7a96b5a895381efb4c3df9
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:14:12 +0700

    add dto CreateMovieRequest

---
+++ src/main/java/com/bayu/movie/dto/CreateMovieRequest.java
@@ -0,0 +1,24 @@
+package com.bayu.movie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMovieRequest {
+
+    @NotBlank(message = "Title must not be blank")
+    private String title;
+
+    private String description;
+
+    private Float rating;
+
+    private String image;
+}
