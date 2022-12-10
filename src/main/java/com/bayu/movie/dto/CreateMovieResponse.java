cf382db5740e4ad59a18bb4769fb93533660ad3d
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:14:23 +0700

    add dto CreateMovieResponse

---
+++ src/main/java/com/bayu/movie/dto/CreateMovieResponse.java
@@ -0,0 +1,27 @@
+package com.bayu.movie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMovieResponse {
+
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private String rating;
+
+    private String image;
+
+    private String createdAt;
+
+    private String updatedAt;
+}
