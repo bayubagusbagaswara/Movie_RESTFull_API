51244a66a3c602765566b2ebec815016253b21dd
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-10 22:19:04 +0700

    add controller ErrorController

---
+++ src/main/java/com/bayu/movie/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.bayu.movie.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    // Bad Request validation
+
+    // Resource Not Found
+
+    // Internal Server Error
+
+}
