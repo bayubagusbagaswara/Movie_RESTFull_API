e7f0d6be7410cbd60a6714bf7e573fba54b0ae99
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 10:33:40 +0700

    update dto CreateMovieRequest

--- src/main/java/com/bayu/movie/dto/CreateMovieRequest.java
+++ src/main/java/com/bayu/movie/dto/CreateMovieRequest.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Data
 @Builder
@@ -14,10 +14,15 @@ import javax.validation.constraints.NotBlank;
 public class CreateMovieRequest {
 
     @NotBlank(message = "Title must not be blank")
+    @Size(min = 3, message = "Title length minimum must be 3 characters")
     private String title;
 
+    @NotBlank(message = "Description must not be blank")
+    @Size(min = 10, max = 20, message = "Description length minimum must be 10 characters and maximum must be 20 characters")
     private String description;
 
+    @NotNull(message = "Rating must not be blank")
+    @Min(value = 1, message = "Rating must be greater than or equal to 1")
     private Float rating;
 
     private String image;
