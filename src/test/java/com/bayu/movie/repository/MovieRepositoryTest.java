51b870003bf464ed648f111f052046466e7c2119
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 10:57:11 +0700

    update test MovieRepositoryTest

--- src/test/java/com/bayu/movie/repository/MovieRepositoryTest.java
+++ src/test/java/com/bayu/movie/repository/MovieRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -42,19 +44,59 @@ class MovieRepositoryTest {
 
     @Test
     void getMovieById() {
+        Integer id = 2;
+        Optional<Movie> movie = movieRepository.findById(id);
+
+        assertTrue(movie.isPresent(), "Product is present with id : " + id);
+    }
+
+    @Test
+    void getMovieByIdNotFound() {
+        Integer id = 5;
+        Optional<Movie> movie = movieRepository.findById(id);
+
+        assertFalse(movie.isPresent(), "Product is not found with id : " + id);
     }
 
     @Test
     void getAllMovies() {
+        List<Movie> movieList = movieRepository.findAll();
+        assertEquals(2, movieList.size());
     }
 
     @Test
     void updateMovie() {
+        Integer id = 2;
 
+        Optional<Movie> movie = movieRepository.findById(id);
+        assertTrue(movie.isPresent(), "Movie is present with id : " + id);
+
+        Movie movieUpdate = movie.get();
+
+        movieUpdate.setTitle("Movie 2 update");
+        movieUpdate.setDescription("Description 2 update");
+        movieUpdate.setRating(9f);
+        movieUpdate.setImage("image-2-update");
+        movieUpdate.setUpdatedAt(LocalDateTime.now());
+
+        movieRepository.save(movieUpdate);
+
+        assertSame(movie.get().getId(), movieUpdate.getId());
+        assertNotNull(movieUpdate.getUpdatedAt());
+        assertNotEquals(movie.get().getCreatedAt(), movieUpdate.getUpdatedAt());
     }
 
     @Test
     void deleteMovie() {
+        Integer id = 3;
+        Optional<Movie> movieOptional = movieRepository.findById(id);
+        assertTrue(movieOptional.isPresent(), "Movie is present");
 
+        Movie movie = movieOptional.get();
+        movieRepository.delete(movie);
+
+        Optional<Movie> movieDeleted = movieRepository.findById(id);
+        assertFalse(movieDeleted.isPresent(), "Movie not found");
     }
+
 }
