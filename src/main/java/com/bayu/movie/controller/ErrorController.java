321b0b80489ef8bb4ac61cc087e321b263b42998
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 18:36:28 +0700

    update controller ErrorController

--- src/main/java/com/bayu/movie/controller/ErrorController.java
+++ src/main/java/com/bayu/movie/controller/ErrorController.java
@@ -1,33 +1,36 @@
 package com.bayu.movie.controller;
 
 import com.bayu.movie.dto.MessageResponse;
+import com.bayu.movie.dto.WebResponse;
 import com.bayu.movie.exception.AppException;
 import com.bayu.movie.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorController {
 
-    @ExceptionHandler(value = ConstraintViolationException.class)
-    @ResponseBody
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ResponseEntity<MessageResponse> validatorHandler(ConstraintViolationException constraintViolationException) {
-        MessageResponse messageResponse = MessageResponse.builder()
-                .success(Boolean.FALSE)
-                .message(constraintViolationException.getMessage())
-                .build();
-        return new ResponseEntity<>(messageResponse, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<WebResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(new WebResponse<>(Boolean.FALSE, "Bad Request", errors), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = ResourceNotFoundException.class)
-    @ResponseBody
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public ResponseEntity<MessageResponse> resourceNotFoundHandler(ResourceNotFoundException resourceNotFoundException) {
         MessageResponse messageResponse = MessageResponse.builder()
@@ -38,7 +41,6 @@ public class ErrorController {
     }
 
     @ExceptionHandler(AppException.class)
-    @ResponseBody
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<MessageResponse> appException(AppException exception) {
         MessageResponse messageResponse = MessageResponse.builder()
