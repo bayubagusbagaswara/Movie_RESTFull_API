125b89506ade2597648b2827a923478b7e138125
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 10:31:53 +0700

    update schema migrate

--- src/main/resources/db/migration/V20221201__Movies_Schema.sql
+++ src/main/resources/db/migration/V20221201__Movies_Schema.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE movies (
-    id integer NOT NULL primary key,
+    id serial NOT NULL primary key,
     title character varying(255) NOT NULL,
     description character varying(255),
     image character varying(255),
