e548862c94bfa2368f5d5ef5f93e083969921008
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-11 10:33:10 +0700

    update repository MovieRepository

--- src/main/java/com/bayu/movie/repository/MovieRepository.java
+++ src/main/java/com/bayu/movie/repository/MovieRepository.java
@@ -2,8 +2,14 @@ package com.bayu.movie.repository;
 
 import com.bayu.movie.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    @Query("SELECT m FROM Movies m WHERE m.title = :title")
+    Optional<Movie> findByTitle(String title);
 }
