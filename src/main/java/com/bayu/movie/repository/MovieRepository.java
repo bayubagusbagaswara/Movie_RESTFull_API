a850d27d3de7078a8d6b0a53d813fb2f086bc04f
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-14 20:43:55 +0700

    add method findByTitleIgnoreCase and findByTitleContainsIgnoreCase

--- src/main/java/com/bayu/movie/repository/MovieRepository.java
+++ src/main/java/com/bayu/movie/repository/MovieRepository.java
@@ -4,6 +4,13 @@ import com.bayu.movie.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    Optional<Movie> findByTitleIgnoreCase(String title);
+
+    List<Movie> findByTitleContainsIgnoreCase(String title);
 }
